842d22d9eb5af9c517111be06a8c6db6b3e8a105
Andrey Kudinov <2499931@gmail.com>
2022-03-10 18:55:41 +0400

    add multi-page for vite

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html')
+      }
+    }
+  }
+})

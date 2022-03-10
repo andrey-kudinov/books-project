8ee1f111d23edcb9cc21575325d1f4f3cafc7a6c
Andrey Kudinov <2499931@gmail.com>
2022-03-10 18:40:13 +0400

    add navigation script

---
+++ scripts/navigation.js
@@ -0,0 +1,10 @@
+const navItems = document.querySelectorAll('.nav__item')
+
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navItems.forEach(item => {
+      item.classList.remove('active')
+    })
+    item.classList.add('active')
+  })
+})

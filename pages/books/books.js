992ce272f0d86b37b7736bb3d6480ba641c066fc
Andrey Kudinov <2499931@gmail.com>
2022-03-12 23:59:25 +0400

    add part of logic to book page, add styles

--- pages/books/books.js
+++ pages/books/books.js
@@ -1,3 +1,8 @@
+import { getBooks, getAuthors } from '../../scripts/airtable'
+import { handleUploadImage } from '../../scripts/cloudinary'
+
+import './books.scss'
+
 //
 // Add image
 //
@@ -35,3 +40,86 @@ const handleAddBook = (file) => {
 }
 
 handleAddBook()
+
+//
+// create books list
+//
+const createBooksHtml = async () => {
+  const books = document.querySelector('.books-page .books')
+  if (!books) return
+
+  const booksData = await getBooks()
+  const authorsData = await getAuthors()
+
+  console.log(booksData)
+  console.log(authorsData)
+
+  books.innerHTML = booksData
+    .map(book => {
+      const author = authorsData.find(
+        author => author.id === book.fields.Author[0]
+      )
+
+      return `
+        <div class="book">
+          <div class="book__img">
+            <img src=${
+              book.fields['Cover Photo']
+                ? book.fields['Cover Photo'][0].thumbnails.large.url
+                : ''
+              } alt='${book.fields.Name}' 
+            />
+          </div>
+
+          <span class="book__title">${book.fields.Name}</span>
+
+          <div class="book__synopsis">
+            <p>${book.fields.Synopsis}</p>
+          </div>
+
+          <span class="book__author">${author.fields.Name}</span>
+
+          <button class="books-page__button book__remove">Remove book</button>
+        </div>
+      `
+    })
+    .join('')
+}
+
+//
+// remove book
+//
+const removeBook = () => {
+  const buttons = document.querySelectorAll('.book__remove')
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      button.closest('.book').remove()
+    })
+  })
+}
+
+//
+// add upload-widget script (for Vite issue)
+//
+const addUploadWidgetScript = () => {
+  const script = document.createElement('script');
+  script.setAttribute('src', 'https://upload-widget.cloudinary.com/global/all.js')
+  script.setAttribute('type', 'text/javascript')
+  document.head.prepend(script)
+}
+
+//
+// async start books page
+//
+const startBooksPage = async () => {
+  if (!document.querySelector('.books-page')) return
+  addUploadWidgetScript()
+  await createBooksHtml()
+  handleUploadImage('.add-book__upload-image')
+  document.querySelector('.books-page').style.opacity = 1
+  removeBook()
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  startBooksPage()
+});

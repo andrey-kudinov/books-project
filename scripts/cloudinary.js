50e2d43ae26de5ae2a5daba32fef572620e59099
Andrey Kudinov <2499931@gmail.com>
2022-03-12 23:58:23 +0400

    refactor

--- scripts/cloudinary.js
+++ scripts/cloudinary.js
@@ -1,20 +1,22 @@
-const myWidget = cloudinary.createUploadWidget(
-  {
-    cloudName: import.meta.env.VITE_CLOUD_NAME,
-    uploadPreset: import.meta.env.VITE_PRESET
-  },
-  (error, result) => {
-    if (!error && result && result.event === 'success') {
-      console.log('Done! Here is the image info: ', result.info)
+export const handleUploadImage = (selector) => {
+  const myWidget = cloudinary.createUploadWidget(
+    {
+      cloudName: import.meta.env.VITE_CLOUD_NAME,
+      uploadPreset: import.meta.env.VITE_PRESET
+    },
+    (error, result) => {
+      if (!error && result && result.event === 'success') {
+        console.log('Done! Here is the image info: ', result.info)
+      }
     }
-  }
-)
+  )
 
-const button = document.querySelector('.add-book__upload-image')
-button.addEventListener(
-  'click',
-  () => {
-    myWidget.open()
-  },
-  false
-)
+  const button = document.querySelector(selector)
+  button.addEventListener(
+    'click',
+    () => {
+      myWidget.open()
+    },
+    false
+    )
+}
